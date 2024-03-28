b73604e65c0450ff8d7d8fffbd4290bb80b22d65
Kwasi Owusu-Donkor <kwasi.donkor@hubtel.com>
2024-03-28 03:33:17 +0000

    Refactor authentication providers and options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,60 +1,7 @@
-import bcrypt from 'bcrypt';
-import NextAuth, { AuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import prisma from '../../../libs/prismadb';
+import NextAuth from 'next-auth';
+import { authOptions } from '@/app/libs/authOptions';
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' }
-      }, 
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Please enter your email and password.');
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        });
-
-        if (!user?.hashedPassword) {
-          throw new Error('Invalid email or password.');
-        }
-
-        const isValid = await bcrypt.compare(credentials.password, user.hashedPassword);
-
-        if (!isValid) {
-          throw new Error('Invalid email or password.');
-        }
-
-        return user;
-      } 
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: 'jwt',
-  }
-};
 
 const handler = NextAuth(authOptions);
 
